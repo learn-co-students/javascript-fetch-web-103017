e5645456c9b8f74d68b60dc90b9f0d21bebbdfd3
psarma89 <priyam.sarma@gmail.com>
2017-12-14 18:06:18 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 const app = "I don't do much.";
+
+// fetch('https://api.github.com/repos/jquery/jquery/commits')
+//   .then(res => res.json())
+//   .then(json => console.log(json));
+
+  const token = '80323965de992d44111d30eb6bfb105a62fbc5b6'
+  fetch('https://api.github.com/user/repos', {
+    headers: {
+      Authorization: `token ${token}`
+    }
+  }).then(res => res.json()).then(json => console.log(json));
