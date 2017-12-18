2ad300342fbe3aa18c98998bf1064099c7a6de33
Silvia Chung <silviachung@Silvias-MacBook.wireless.newschool.edu>
2017-12-17 19:55:55 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const app = "I don't do much.";
+const token = '85272460f6fdfe6df8ab39edf471a44fbbd7e506';
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`;
+  }
+}).then(res => res.json()).then(json => console.log(json));
